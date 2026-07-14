f7d67465f489893bfa616299274ccf47e2f24ea7
Beno <kelvin.beno.k@gmail.com>
2026-07-14 13:16:32 +0300

    Additions of prisma

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors"
+
+
+
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/health", (req, res) => {
+
+    res.json({
+
+        status: "Chess server running"
+    })
+
+
+
+})
+
+export default app;
+
+
+
+
+
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,10 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-
-app.get("/health", (req, res) => {
-    res.json({
-        status: "Chess server running"
-    });
-});
-
-
 const PORT = process.env.PORT || 4000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
